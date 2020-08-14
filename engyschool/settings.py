cc73828fa028148924fe6f5076ea356fcf4cad61
EakzIT <pilpaff@gmail.com>
2020-08-14 14:43:11 +0300

    lucky study reworks

--- engyschool/settings.py
+++ engyschool/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'engyschool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'engy',
-        'USER': 'ronaldduck',
-        'PASSWORD': 'donaldduck',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
