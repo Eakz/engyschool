60348c63f4b0a91bd10079ff9135b1e7892cca3d
EakzIT <pilpaff@gmail.com>
2020-08-27 18:50:30 -0500

    zzz

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Faq, Dood
+from .models import Dood
 
 # Register your models here.
-admin.site.register(Faq)
 admin.site.register(Dood)
--- main/views.py
+++ main/views.py
@@ -2,15 +2,16 @@ import os
 from django.conf import settings
 from django.shortcuts import render
 from django.templatetags.static import static
-from .models import Faq
+# from .models import Faq
 
 # Create your views here.
 
 
 def index(request):
-    faqs = Faq.objects.all()[0:6]
-    faqs_up = faqs[0:3]
-    faqs_down = faqs[3:6]
+    # faqs = Faq.objects.all()[0:6]
+    # faqs_up = faqs[0:3]
+    # faqs_down = faqs[3:6]
     path = settings.MEDIA_ROOT
-    context = {'images': path, 'faqs_up': faqs_up, 'faqs_down': faqs_down}
+    # context = {'images': path, 'faqs_up': faqs_up, 'faqs_down': faqs_down}
+    context = {}
     return render(request, "main/index.html", context)
