daf858a2201d0118903611d0359e3131e60857c0
EakzIT <pilpaff@gmail.com>
2020-09-06 14:25:45 +0300

    faq fix

--- main/views.py
+++ main/views.py
@@ -8,10 +8,9 @@ from django.templatetags.static import static
 
 
 def index(request):
-    # faqs = Faq.objects.all()[0:6]
-    # faqs_up = faqs[0:3]
-    # faqs_down = faqs[3:6]
+    faqs = Faq.objects.all()[0:6]
+    faqs_up = faqs[0:3]
+    faqs_down = faqs[3:6]
     path = settings.MEDIA_ROOT
-    # context = {'images': path, 'faqs_up': faqs_up, 'faqs_down': faqs_down}
-    context = {}
+    context = {'images': path, 'faqs_up': faqs_up, 'faqs_down': faqs_down}
     return render(request, "main/index.html", context)
